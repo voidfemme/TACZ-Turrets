5af1ba6883f3ed7f13e5173a066bc15ddbec379c
Entropy159 <115195040+Entropy159@users.noreply.github.com>
2026-03-10 14:22:34 -0600

    Fix semicolon

--- src/main/java/com/entropy/tacz_turrets/common/entity/TaczShootAttack.java
+++ src/main/java/com/entropy/tacz_turrets/common/entity/TaczShootAttack.java
@@ -34,7 +34,7 @@ public class TaczShootAttack<E extends TurretEntity> extends ExtendedBehaviour<E
     @Override
     protected boolean checkExtraStartConditions(@NotNull ServerLevel level, @NotNull E entity) {
         this.target = BrainUtils.getTargetOfEntity(entity);
-        return target != null && BrainUtils.canSee(entity, this.target) && !target.getUUID().equals(entity.owner)
+        return target != null && BrainUtils.canSee(entity, this.target) && !target.getUUID().equals(entity.owner);
     }
 
     @Override
